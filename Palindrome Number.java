9c3b78b9f63da7369e3b3c2eccecc22b8faaba10
arthjani <arthjani3@gmail.com>
2022-02-28 01:39:58 -0500

    Palindrome Number

--- Palindrome Number.java
+++ Palindrome Number.java
@@ -13,3 +13,17 @@ class Solution {
         return x==rev;
     }
 }
+
+//or 
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0 || (x%10==0 && x!=0))return false;
+        int revert =0 ;
+        while(x>revert){
+            revert = revert *10 + x%10;
+            x/=10;
+        }
+        return x==revert || x==revert/10;
+    }
+}
