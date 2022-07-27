909f06f936193d77dc713fdb393525e81437e34a
arthjani <arthjani3@gmail.com>
2022-07-27 17:41:39 -0400

    Friends Pairing Problem -- Dynamic Programming

---
+++ Friends Pairing Problem Dynamic Programming.java
@@ -0,0 +1,23 @@
+//Friends Pairing Problem -- Dynamic Programming
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main{
+
+
+	public static void main(String[] args) throws Exception{
+		Scanner sc  = new Scanner(System.in);
+
+		int n=sc.nextInt();
+		
+		int[] dp = new int[n+1];
+		dp[1]=1;
+		dp[2]=2;
+		for(int i=3;i<dp.length;i++){
+			dp[i]=dp[i-1]+(i-1)*dp[i-2];
+		}
+		System.out.println(dp[n]);
+		}
+	}
