a8850f32e871a750d65e60868eb9d28e4664a2d3
arthjani <arthjani3@gmail.com>
2022-08-01 14:41:51 -0400

    Longest Increasing Subsequence--Dynamic Programming

---
+++  Longest Increasing Subsequence--Dynamic Programming.java
@@ -0,0 +1,41 @@
+// Longest Increasing Subsequence--Dynamic Programming
+
+
+import java.util.*;
+import java.io.*;
+
+class Main{
+
+	public static void solution(int[] arr){
+     int[] dp = new int[arr.length];
+     dp[0]=1;
+     int result=0;
+     for(int i=0;i<dp.length;i++){
+        int max=0;
+     	for(int j=0;j<i;j++){
+          if(arr[j] <= arr[i]){
+          	if(dp[j]>max){
+          		max=dp[j];
+          	}
+          }
+     	}
+     	dp[i]=max+1;
+     	if(dp[i]>result){
+     		result=dp[i];
+     	}
+     }
+     System.out.println(result);
+
+	}
+	public static void main(String[] args){
+		Scanner sc  = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+
+		solution(arr);
+	}
+}
